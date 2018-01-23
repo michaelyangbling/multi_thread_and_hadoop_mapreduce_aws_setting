09b4bcb622c33776864ab2e28398942c0377a4f3
minemine <yang.zho@husku.neu.edu>
2018-01-23 14:50:07 -0500

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -59,7 +59,39 @@ class processor {
         for (String key : count_sum.keySet()) {
             results.put(key, count_sum.get(key)[1] / count_sum.get(key)[0]);
         }
+
         return results;
+
+    }
+    //public static Map<>
+    public static Map<String, Float> divide(Map<String, float[]> count_sum){
+        Map<String, Float> results = new HashMap<String, Float>();
+        for (String key : count_sum.keySet()) {
+            results.put(key, count_sum.get(key)[1] / count_sum.get(key)[0]);
+        }
+        return results;
+    }
+}
+
+class runner implements Runnable{
+    public int start, end;  public static List<List<String>> listOfList;
+    public static Map<String, float[]> count_sum=new HashMap<String, float[]>();
+    runner(int x, int y){
+        start=x;end=y;
+    }
+    public void run() {
+        float valuex[];
+
+        for (int i = start; i <=end; i++) {
+            if (count_sum.get(listOfList.get(i).get(0)) == null) {
+                valuex = new float[]{1, Float.parseFloat(listOfList.get(i).get(1))};
+                count_sum.put(listOfList.get(i).get(0), valuex);
+            } else {
+                float[] value = count_sum.get(listOfList.get(i).get(0));
+                valuex = new float[]{1 + value[0], Float.parseFloat(listOfList.get(i).get(1)) + value[1]};
+                count_sum.put(listOfList.get(i).get(0), valuex);
+            }
+        }
 
     }
 }
@@ -69,12 +101,33 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> weather=processor.load("/Users/yzh/Downloads/1912.csv");
-        List<List<String>> tmax=processor.filter_tmax(weather);
-        Map<String, Float> results=processor.get_mean(tmax);
-
+        System.out.println("split");
+        Map<String, Float> results=new HashMap<String, Float>();
+        long start=System.currentTimeMillis();
+        for(int i=0; i<=3;i++)
+        {results=processor.get_mean(processor.filter_tmax(weather));}
+        System.out.println(System.currentTimeMillis()-start);
         System.out.println(results);
+
+        start=System.currentTimeMillis();
+        List<List<String>> listOfList;
+        for(int i=0; i<=3;i++){
+        listOfList=processor.filter_tmax(weather); runner.listOfList=listOfList;
+        Thread t1=new Thread( new runner(0,listOfList.size()/2) );
+        Thread t2=new Thread( new runner(1 + listOfList.size()/2,listOfList.size()-1 ));
+        t1.start();
+        try{ t1.join();t2.join();}
+        catch(Exception xx) { System.out.println("error2");}
+        results=processor.divide(runner.count_sum);}
+        System.out.println(System.currentTimeMillis()-start);
+        System.out.println(results);
+
+        }
+
 
 
     }
-}
 
+
+
+//txt results:{CA006119064=-11.0, USC00364972=22.0, USC00392207=-76.666664}
